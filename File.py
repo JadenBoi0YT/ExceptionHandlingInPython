42533a770d92e2169a7462d7e8615ba0143ee8bc
JadenBoi0YT <86737376+JadenBoi0YT@users.noreply.github.com>
2022-01-19 17:55:43 +0000

    Update File.py

--- File.py
+++ File.py
@@ -8,21 +8,29 @@ root.geometry("600x400")
 
 root.configure(background="yellow")
 
+img = ImageTk.PhotoImage(Image.open("Card.png"))
+
+CardImage = Label(root,image=img)
+CardImage.pack()
+
 input_box = Entry(root)
 input_box.pack()
 
-img = ImageTk.PhotoImage(Image.open("Card.png"))
-
-cardLabel = Label(root, image=img)
-cardLabel.pack()
-
 def authentication():
     inputedData = input_box.get()
-    print(type(inputedData))
     
+    try:
+        inputedData = int(input_box.get())
+        inputdata = type(inputedData)
+        print(inputdata)
+        messagebox.showinfo("Card","card is accepted")
+    except:
+        messagebox.showinfo("Card","card is rejected")
+
+
     #if inputedData == 202212345:
         #messagebox.showinfo("CreditCard","Credit Card Accepted")
 btn=Button(root,text="Check",command=authentication)
 btn.pack()
-        
-root.mainloop()
+
+root.mainloop() 
